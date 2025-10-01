f46e91d944b2d644ad7060d4eb3de2c963ebaac5
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2025-10-01 11:30:58 +0000

    Deploying to gh-pages from @ Ivan227-hub/goit-js-hw-09@6dc791822792ded9e2f8f9954e6e8393983166f1 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,11 +1,11 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as i}from"./assets/vendor-CgTBfC_f.js";const o=[{preview:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",original:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",description:"Tree in sunset"},{preview:"https://cdn.pixabay.com/photo/2016/02/19/11/19/aurora-1209686__340.jpg",original:"https://cdn.pixabay.com/photo/2016/02/19/11/19/aurora-1209686_1280.jpg",description:"Aurora Borealis"},{preview:"https://cdn.pixabay.com/photo/2015/09/18/11/47/bird-944887__340.jpg",original:"https://cdn.pixabay.com/photo/2015/09/18/11/47/bird-944887_1280.jpg",description:"Flying bird"}],r=document.querySelector(".gallery"),p=o.map(a=>`
-  <li class="gallery-item">
-    <a class="gallery-link" href="${a.original}">
-      <img 
-        class="gallery-image"
-        src="${a.preview}" 
-        alt="${a.description}" 
-      />
-    </a>
-  </li>`).join("");r.innerHTML=p;new i(".gallery a",{captionsData:"alt",captionDelay:250});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as t}from"./assets/vendor-CgTBfC_f.js";const p=[{preview:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",original:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",description:"Tree in sunset"},{preview:"https://cdn.pixabay.com/photo/2016/02/19/11/19/aurora-1209686__340.jpg",original:"https://cdn.pixabay.com/photo/2016/02/19/11/19/aurora-1209686_1280.jpg",description:"Aurora Borealis"},{preview:"https://cdn.pixabay.com/photo/2015/09/18/11/47/bird-944887__340.jpg",original:"https://cdn.pixabay.com/photo/2015/09/18/11/47/bird-944887_1280.jpg",description:"Flying bird"}],r=document.querySelector(".gallery"),e=p.map(({preview:a,original:i,description:o})=>`
+    <li class="gallery-item">
+      <a class="gallery-link" href="${i}">
+        <img 
+          class="gallery-image" 
+          src="${a}" 
+          alt="${o}" 
+        />
+      </a>
+    </li>`).join("");r.innerHTML=e;new t(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
